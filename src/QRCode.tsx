f830f3b7bcae326e68b7ba9817bdebe8cfc2f56f
Terratical <lakonimangil@gmail.com>
2026-04-03 12:25:45 +0000

    refactor index.css to rename background variable and update QRCode component styles for improved layout

--- src/QRCode.tsx
+++ src/QRCode.tsx
@@ -3,13 +3,13 @@ import QRCodeImg from "@/assets/image-qr-code.png";
 function QRCode() {
   return (
     <main className="flex h-screen items-center justify-center">
-      <figure className="max-w-[min(80%,320px)] rounded-2xl bg-white p-3">
+      <figure className="max-w-[20rem] rounded-2xl bg-white px-(--spacing-sm) pt-(--spacing-sm)">
         <img
           src={QRCodeImg}
           alt="QR code linking to frontend mentor"
           className="h-auto w-full rounded-2xl"
         ></img>
-        <figcaption className="flex flex-col gap-2 py-4 text-center">
+        <figcaption className="mt-(--spacing-md) mb-(--spacing-lg) flex flex-col gap-3 px-4 text-center">
           <h1 className="card-title">
             Improve your front-end skills by building projects
           </h1>
