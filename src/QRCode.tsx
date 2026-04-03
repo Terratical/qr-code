e9e2ff526171caa9f5410f9ad0b980f02aaf0e22
Terratical <lakonimangil@gmail.com>
2026-04-03 11:53:27 +0000

    update styles, enhance QRCode component, and modify Vite metadata; remove unused utils file

--- src/QRCode.tsx
+++ src/QRCode.tsx
@@ -2,10 +2,14 @@ import QRCodeImg from "@/assets/image-qr-code.png";
 
 function QRCode() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <main className="max-w-[min(80%,320px)] rounded-2xl bg-white p-3">
-        <img src={QRCodeImg} alt="QRCode Image" className="h-auto w-full rounded-2xl"></img>
-        <div className="flex flex-col gap-2 py-4 text-center">
+    <main className="flex h-screen items-center justify-center">
+      <figure className="max-w-[min(80%,320px)] rounded-2xl bg-white p-3">
+        <img
+          src={QRCodeImg}
+          alt="QR code linking to frontend mentor"
+          className="h-auto w-full rounded-2xl"
+        ></img>
+        <caption className="flex flex-col gap-2 py-4 text-center">
           <h1 className="card-title">
             Improve your front-end skills by building projects
           </h1>
@@ -13,9 +17,9 @@ function QRCode() {
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
           </p>
-        </div>
-      </main>
-    </div>
+        </caption>
+      </figure>
+    </main>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import QRCode from './QRCode.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import QRCode from "./QRCode";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QRCode />
   </StrictMode>,
-)
+);
